ed54f7e6db58b22ed312ebb2383fdf486fa08de9
ThihaSoe10 <thihasoe102000@gmail.com>
2024-07-07 12:47:28 +0630

    four

--- src/firebaseFunctions.tsx
+++ src/firebaseFunctions.tsx
@@ -26,56 +26,18 @@ export const updateUserAutoIncrementInFirebase = (
   });
 };
 
-// Function to record a referral
-export const recordReferral = async (
-  referrerUserId: string,
-  newUserId: string
-) => {
-  if (!referrerUserId || !newUserId) return;
-
-  try {
-    const referralRef = ref(db, `referrals/${referrerUserId}/referredUsers`);
-    const snapshot = await get(referralRef);
-
-    // Check if referredUsers node exists
-    if (snapshot.exists()) {
-      const referredUsers = snapshot.val();
-      referredUsers[newUserId] = true;
-      update(referralRef, referredUsers);
-    } else {
-      // If referredUsers node doesn't exist, create it
-      set(referralRef, {
-        [newUserId]: true,
-      });
-    }
-
-    // Update referral count
-    const referralCountRef = ref(
-      db,
-      `referrals/${referrerUserId}/referralCount`
-    );
-    const referralCountSnapshot = await get(referralCountRef);
-    let newCount = 1;
-    if (referralCountSnapshot.exists()) {
-      newCount = referralCountSnapshot.val() + 1;
-    }
-    set(referralCountRef, newCount);
-  } catch (error) {
-    console.error("Error recording referral:", error);
-  }
-};
-
-// Function to get the count of referred users
-export const getReferredUsersCount = async (userId: string) => {
+export const getReferredUsersCount = async (userId: string): Promise<number> => {
   if (!userId) return 0;
 
   try {
-    const referralCountRef = ref(db, `referrals/${userId}/referralCount`);
-    const snapshot = await get(referralCountRef);
+    const referralsRef = ref(db, "referrals/" + userId);
+    const snapshot = await get(referralsRef);
+
     if (snapshot.exists()) {
-      return snapshot.val(); // Return the referral count
+      const referralsData = snapshot.val();
+      return Object.keys(referralsData).length;
     } else {
-      return 0; // No referrals found
+      return 0;
     }
   } catch (error) {
     console.error("Error fetching referred users count:", error);
